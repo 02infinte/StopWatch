cab4ab308b2df4265d450c52515c4b2cbe4fc239
Sifaty <sifatybd223@gmail.com>
2025-02-02 19:49:47 +0600

    padStart added

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -32,6 +32,11 @@ const StopWatch = () => {
     let minutes = Math.floor(elapsedTime/(1000 *60)% 60);
     let seconds = Math.floor(elapsedTime/(1000)% 60);
     let milliseconds = Math.floor(elapsedTime%(1000)/ 10);  //divided by 10 to display only the first two digits
+
+    hours= String(hours).padStart(2,"0");
+    minutes= String(minutes).padStart(2,"0");
+    seconds= String(seconds).padStart(2,"0");
+    milliseconds= String(milliseconds).padStart(2,"0");
      return `${hours} : ${minutes} : ${seconds} : ${milliseconds}`;
   }
   return (
