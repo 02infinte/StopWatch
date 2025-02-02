415dd1379f1e5738694c7ca5055e7af1523f1f10
Sifaty <sifatybd223@gmail.com>
2025-02-02 19:40:47 +0600

    functionality done

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -9,18 +9,35 @@ const StopWatch = () => {
   const intervalIdRef = useRef(null);
   const startTimeRef = useRef(0);
   useEffect(()=>{
-
+    if(isRunning){
+     intervalIdRef.current= setInterval(()=>{
+        setElapsedTime(Date.now()-startTimeRef.current);
+      },10)
+    }
+    return ()=>{
+      clearInterval(intervalIdRef.current);
+    }
   },[isRunning])
   const start=()=>{
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+  }
 
+  const stop=()=>{
+    setIsRunning(false);
+    setElapsedTime(0);
   }
   const formatTime =()=>{
-     return `00:00:00`
+    let hours = Math.floor(elapsedTime/(1000 *60 * 60 ));
+    let minutes = Math.floor(elapsedTime/(1000 *60)% 60);
+    let seconds = Math.floor(elapsedTime/(1000)% 60);
+    let milliseconds = Math.floor(elapsedTime%(1000)/ 10);  //divided by 10 to display only the first two digits
+     return `${hours} : ${minutes} : ${seconds} : ${milliseconds}`;
   }
   return (
     <div>
       <h1>{formatTime()}</h1>
-      <button></button>
+      <button onClick={isRunning ?  stop : start} >{isRunning ? "Stop" : "Start"}</button>
     </div>
   );
 };
